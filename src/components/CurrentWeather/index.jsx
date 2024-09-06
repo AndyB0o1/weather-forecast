5a3385c6410312b3e8940f8db7884d9aeb282011
Andy Bool <andybool50@gmail.com>
2024-09-06 11:45:51 +0100

    added changing background to current weather

--- src/components/CurrentWeather/index.jsx
+++ src/components/CurrentWeather/index.jsx
@@ -7,14 +7,17 @@ export default function Weather() {
     const [country, setCountry] = useState('UK');
     const [weather, setWeather] = useState(null);
     const [umbrella, setUmbrella] = useState('');
+    const [background, setBackground] = useState('');
     const [lon, setLon] = useState();
     const [lat, setLat] = useState();
+    const apiKey = import.meta.env.VITE_API_KEY;
 
     function getWeather() {
-        const apiKey = import.meta.env.VITE_API_KEY;
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`;
         fetch(url).then((response) => response.json())
             .then((data) => {
+
+                console.log(data)
 
                 let maxTemp = Math.round(data.main.temp);
                 let feelsLike = Math.round(data.main.feels_like);
@@ -31,36 +34,46 @@ export default function Weather() {
                     icon: data.weather[0].icon
                 }
                 setWeather(currentWeather);
-                if (precipitation >= 200 && precipitation <= 622) {
+                if (precipitation <= 622) {
+                    setBackground("bg-[url('https://i.pinimg.com/474x/43/02/fb/4302fb8632ea3bd5db5f175c0c1886c0.jpg')] p-1.5")
                     setUmbrella("You're going to need an umbrella today!")
                 }
-                else {
-                    setUmbrella("No umbrella needed today!")
+                else if (precipitation === 800) {
+                    setBackground("bg-[url('https://images.pexels.com/photos/281260/pexels-photo-281260.jpeg?auto=compress&cs=tinysrgb&w=800')] p-1.5")
+                    setUmbrella("Definitely no umbrella needed today, but you might want some suncream!")
                 }
+                else {
+                    setBackground("bg-[url('https://images.pexels.com/photos/414659/pexels-photo-414659.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')] p-1.5")
+                    setUmbrella("You probably don't need an umbrella today")
+                } 
             })
     }
+    console.log(background)
+
     return (
         <div className="p-2 mx-auto md:mx-none">
             <h1 className="p-2 md:text-2xl">Enter your closest city below to see the current weather conditions</h1>
             <input className="p-1.5 border rounded" type="text" placeholder="Enter closest city name" value={city} onChange={(e) => setCity(e.target.value)}></input>
             <input className="mt-2 md:mt-0 sm:ml-2 p-1.5 border rounded" type="text" placeholder="UK" value={country} onChange={(e) => setCountry(e.target.value)}></input>
-            <button className="mt-2 sm:ml-2 md:mt-0 p-2 bg-amber-500 font-bold text-white rounded" onClick={getWeather}>See weather</button>
-            <div className="bg-[url('https://i.pinimg.com/474x/43/02/fb/4302fb8632ea3bd5db5f175c0c1886c0.jpg')] p-1.5">
-                {weather && (
-                    <div className="p-2">
-                        <div className="weather p-2 border-2 md:border-4 border-amber-500 rounded-lg md:text-xl">
-                            <h2 className="md:m-0.5 p-0.5 bg-amber-100 text-cyan-900 rounded ">{weather.location}</h2>
-                            <div className="flex p-1.5 rounded">
-                                <img className="p-1.5" src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt={`${weather.condition}`}></img>
-                                <p className="my-0.5 p-1.5 bg-amber-500 font-bold text-white rounded ">{umbrella}</p>
+            <button className="my-2 sm:ml-2 md:mt-0 p-2 bg-amber-500 font-bold text-white rounded" onClick={getWeather}>See weather</button>
+            <div className={`${background}`}>
+                <div className="opacity-100">
+                    {weather && (
+                        <div className="p-2">
+                            <div className="weather p-2 border-2 md:border-4 border-amber-500 rounded-lg md:text-xl">
+                                <h2 className="md:m-0.5 p-0.5 text-white drop-shadow-xl rounded ">{weather.location}</h2>
+                                <div className="flex p-1.5 rounded">
+                                    <img className="p-1.5" src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt={`${weather.condition}`}></img>
+                                    <p className="my-0.5 p-1.5 font-bold text-white drop-shadow-xl rounded">{umbrella}</p>
+                                </div>
+                                <p className="m-0.5 p-0.5 text-white drop-shadow-xl rounded ">Current conditions: {weather.condition}</p>
+                                <p className="m-0.5 p-0.5 text-white drop-shadow-xl rounded ">{weather.temperature}&deg;C and feels like {weather.feels_like}&deg;C</p>
+                                <p className="m-0.5 p-0.5 text-white drop-shadow-xl rounded ">{weather.wind}</p>
                             </div>
-                            <p className="m-0.5 p-0.5 bg-amber-100 text-cyan-900 rounded ">Current conditions: {weather.condition}</p>
-                            <p className="m-0.5 p-0.5 bg-amber-100 text-cyan-900 rounded ">{weather.temperature}&deg;C and feels like {weather.feels_like}&deg;C</p>
-                            <p className="m-0.5 p-0.5 bg-amber-100 text-cyan-900 rounded ">{weather.wind}</p>
-                        </div>
-                    </div>)}
+                        </div>)}
+                </div>
             </div>
-            <Forecast lon={lon} lat={lat} />
+            <Forecast apiKey={apiKey} lon={lon} lat={lat} />
         </div >
     )
 }
--- src/components/Forecast/index.jsx
+++ src/components/Forecast/index.jsx
@@ -4,7 +4,7 @@ export default function Forecast({ lon, lat, apiKey }) {
 
 
     function getForecast() {
-        const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
         // const [image, setImage] = useState('');
         // const [description, setDescription] = useState('');
 
@@ -15,7 +15,7 @@ export default function Forecast({ lon, lat, apiKey }) {
     }
     return (
         <>
-            <button className="mt-2 sm:ml-2 md:mt-0 p-2 bg-amber-500 font-bold text-white rounded" onClick={getForecast}>See 5 day forecast</button>
+            <button className="mt-2 sm:ml-2 p-2 bg-amber-500 font-bold text-white rounded" onClick={getForecast}>See 5 day forecast</button>
             {/* <img className="p-1.5" src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt={`${weather.condition}`}></img> */}
         </>
     )
